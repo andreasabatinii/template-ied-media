49e414b58c50b9739d0e99c3cdf1b1678256c1b0
andreasabatinii <a.sabatini@ied.edu>
2024-01-11 10:44:29 +0100

    functions file

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Functions.
+ */
+
+ // Inlcudo foglio di stile principlae
+ wp_enqueue_style( 'style', get_stylesheet_uri( 'style', get_stylesheet_uri(), array(), wp_get_theme()->get('Version'), 'all') );
+?>
