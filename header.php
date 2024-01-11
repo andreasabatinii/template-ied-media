6771f92c15440be9fa473b3e26c7d2b3030db518
andreasabatinii <a.sabatini@ied.edu>
2024-01-11 10:15:07 +0100

    language

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 ?>
 
 <!DOCTYPE html>
-<html <?php langiage_attributes();?>>
+<html <?php language_attributes();?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
